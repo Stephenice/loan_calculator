1e47e86e054b4c4053d799df9d7e4c59002fef85
Stephenice <stephencode@hotmail.com>
2021-01-11 03:22:11 +0100

    Loan calculator

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict'
+const rangeAmount = document.querySelector('.range');
+const rangeValue = document.querySelector('.rangeValue');
+const period = document.querySelector('#monthly');
+const interest = 2.5;
+const monthlyPayment = document.querySelector('.monthlyPayment');
+
+
